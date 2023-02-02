c3efe70d6503338c8903f2f48e321fe21f9d1cd4
xu756 <756334744@qq.com>
2023-02-02 20:27:24 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Home from './pages/Home.vue'
+</script>
+
+<template>
+  <div id="header">
+    三只象可视化数据监控平台
+  </div>
+  <div id="container">
+    <Home/>
+  </div>
+</template>
